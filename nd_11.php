3592d7dc43acf9eb4c74c1c4bbae1d34a836dfca
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-05-06 07:01:14 +0300

    + some lines

--- nd_11.php
+++ nd_11.php
@@ -10,12 +10,30 @@ $d = rand(1000, 9999);
 $e = rand(1000, 9999);
 $f = rand(1000, 9999);
 
-if($a < $b){
-    echo "a:$a, b:$b, c:$c, d:$d, e:$e, f:$f <br>"; 
-}elseif($a < $c){
-    echo "b:$b, a:$a, c:$c, d:$d, e:$e, f:$f <br>";
-}elseif($a < $d){
 
+
+
+
+if($a < $b && $b < $c && $c < $d && $d < $e && $e < $f){
+    echo "a:$a, b:$b, c:$c, d:$d, e:$e, f:$f <br>"; 
+}elseif($b < $c && $c < $d && $d < $e && $e < $f && $f < $a){
+    echo "b:$b, c:$c, d:$d, e:$e, f:$f, a:$a  <br>";
+}elseif($c < $d && $d < $e && $e < $f && $f < $a && $a < $b){
+    echo "c:$c, d:$d, e:$e, f:$f, a:$a, b:$b  <br>";
+} elseif($d < $e && $e < $f && $f < $a && $a < $b && $b < $c){
+    echo "d:$d, e:$e, f:$f, a:$a, b:$b, c:$c  <br>";
+} elseif($e < $f && $f < $a && $a < $b && $b < $c && $c < $d){
+    echo "e:$e, f:$f, a:$a, b:$b, c:$c, d:$d  <br>";
+} elseif($f < $a && $a < $b && $b < $c && $c < $d && $d < $e){
+    echo "f:$f, a:$a, b:$b, c:$c, d:$d, e:$e  <br>";
+} elseif($f < $a && $a < $b && $b < $c && $c < $d && $d < $e){
+    echo "f:$f, a:$a, b:$b, c:$c, d:$d, e:$e  <br>";
+} elseif($b < $a && $a < $e && $e < $c && $c < $d && $d < $f){
+    echo "b:$b, a:$a, e:$e, c:$c, d:$d, f:$f  <br>";
+} elseif($d < $f && $f < $c && $c < $b && $b < $e && $e < $a){
+    echo "d:$d, f:$f, c:$c, b:$b, e:$e, a:$a <br>";
+} elseif($d < $f && $f < $c && $c < $b && $b < $e && $e < $a){
+    echo "d:$d, f:$f, c:$c, b:$b, e:$e, a:$a <br>";
 }     
 
 echo "a:$a, b:$b, c:$c, d:$d, e:$e, f:$f <br>";
