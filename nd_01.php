7b94be7fed3df3040adfba4b7472969400e04438
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-05-09 07:33:34 +0300

    01-fix

--- nd_01.php
+++ nd_01.php
@@ -7,7 +7,9 @@ pavardės kintamuosius atspausdintų tokį sakinį :
 "Aš esu Vardenis Pavardenis. Man yra XX metai(ų)". */
 $vardas = 'Kestutis';
 $pavarde = 'Skrebe';
-$gimMetai = date(1977);
-$thisYear = date("Y");
-$dateDif = date_diff($thisYear-$gimMetai);
+$gimMetai = 1977;
+$thisYear = 2022;
+
+$dateDif = $thisYear - $gimMetai;
 $sentence = "Aš esu $vardas $pavarde. Man yra $dateDif metai(ų)";
+echo $sentence;
